719740f34f7b76ebe7cd615c98bf27ff3d5a1f79
Karol Chojnacki <karol.chojnacki@icloud.copm>
2018-12-18 13:59:39 +0100

    Update Contents.swift
    Added class with initializations

--- ios_zad_1.playground/Contents.swift
+++ ios_zad_1.playground/Contents.swift
@@ -1,3 +1,21 @@
 import Cocoa
 
+class SalaKonf{
+    var name:String = ""
+    var state:String = ""
+    var time:Int = 0
+    
+    //inicjaliacja z rezerwacja
+    init(newName:String, reservation:String, t:Int){
+        name = newName
+        state = reservation
+        time = t
+    }
+    //inicjalizacja bez rezerwacji
+    init(newName:String, reservation:String){
+        name = newName
+        state = reservation
+        time = 0
+    }
+}
 
