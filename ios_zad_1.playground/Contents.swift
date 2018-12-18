07c5e0bcec92474566d63e7600a434ee41608afc
Karol Chojnacki <karol.chojnacki@icloud.copm>
2018-12-18 16:25:40 +0100

    Update Contents.swift
    changed let -> var

--- ios_zad_1.playground/Contents.swift
+++ ios_zad_1.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
-let res:String = "Reserved"
-let nres:String = "NotReserved"
+var res:String = "Reserved"
+var nres:String = "NotReserved"
 
 class SalaKonf{
     var name:String = ""
@@ -30,25 +30,25 @@ var arrayReserved = [SalaKonf]()
 var arrayNotReserved = [SalaKonf]()
 var array = [SalaKonf]()
 
-let s1 = SalaKonf(newName: "Sala nr 1", reservation: res, t: 60)
+var s1 = SalaKonf(newName: "Sala nr 1", reservation: res, t: 60)
 array.append(s1)
-let s2 = SalaKonf(newName: "Sala nr 2", reservation: nres)
+var s2 = SalaKonf(newName: "Sala nr 2", reservation: nres)
 array.append(s2)
-let s3 = SalaKonf(newName: "Sala nr 3", reservation: res, t: 180)
+var s3 = SalaKonf(newName: "Sala nr 3", reservation: res, t: 180)
 array.append(s3)
-let s4 = SalaKonf(newName: "Sala nr 4", reservation: res, t: 120)
+var s4 = SalaKonf(newName: "Sala nr 4", reservation: res, t: 120)
 array.append(s4)
-let s5 = SalaKonf(newName: "Sala nr 5", reservation: nres)
+var s5 = SalaKonf(newName: "Sala nr 5", reservation: nres)
 array.append(s5)
-let s6 = SalaKonf(newName: "Sala nr 6", reservation: res, t: 30)
+var s6 = SalaKonf(newName: "Sala nr 6", reservation: res, t: 30)
 array.append(s6)
-let s7 = SalaKonf(newName: "Sala nr 7", reservation: res, t: 90)
+var s7 = SalaKonf(newName: "Sala nr 7", reservation: res, t: 90)
 array.append(s7)
-let s8 = SalaKonf(newName: "Sala nr 8", reservation: nres)
+var s8 = SalaKonf(newName: "Sala nr 8", reservation: nres)
 array.append(s8)
-let s9 = SalaKonf(newName: "Sala nr 9", reservation: nres)
+var s9 = SalaKonf(newName: "Sala nr 9", reservation: nres)
 array.append(s9)
-let s10 = SalaKonf(newName: "Sala nr 10", reservation: res, t: 300)
+var s10 = SalaKonf(newName: "Sala nr 10", reservation: res, t: 300)
 array.append(s10)
 
 //rozdzielenie do dwóch różnych tablic ze względu na rezerwacje
