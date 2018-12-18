fe873c3beac7ba110e4bab2edb3396aac25cfbd2
Karol Chojnacki <karol.chojnacki@icloud.copm>
2018-12-18 14:27:29 +0100

    Update Contents.swift
    Sorted into 2 arrays depending on state.

--- ios_zad_1.playground/Contents.swift
+++ ios_zad_1.playground/Contents.swift
@@ -1,5 +1,8 @@
 import Cocoa
 
+let res:String = "Reserved"
+let nres:String = "NotReserved"
+
 class SalaKonf{
     var name:String = ""
     var state:String = ""
@@ -19,7 +22,50 @@ class SalaKonf{
     }
     
     func description(){
-        (state == "reserved") ? print("\(name) - \(state) - \(time)") : print("\(name) - \(state)")
+        (state == res) ? print("\(name) - \(state) - \(time)") : print("\(name) - \(state)")
     }
 }
 
+var arrayReserved = [SalaKonf]()
+var arrayNotReserved = [SalaKonf]()
+var array = [SalaKonf]()
+
+let s1 = SalaKonf(newName: "Sala nr 1", reservation: res, t: 60)
+array.append(s1)
+let s2 = SalaKonf(newName: "Sala nr 2", reservation: nres)
+array.append(s2)
+let s3 = SalaKonf(newName: "Sala nr 3", reservation: res, t: 180)
+array.append(s3)
+let s4 = SalaKonf(newName: "Sala nr 4", reservation: res, t: 120)
+array.append(s4)
+let s5 = SalaKonf(newName: "Sala nr 5", reservation: nres)
+array.append(s5)
+let s6 = SalaKonf(newName: "Sala nr 6", reservation: res, t: 30)
+array.append(s6)
+let s7 = SalaKonf(newName: "Sala nr 7", reservation: res, t: 90)
+array.append(s7)
+let s8 = SalaKonf(newName: "Sala nr 8", reservation: nres)
+array.append(s8)
+let s9 = SalaKonf(newName: "Sala nr 9", reservation: nres)
+array.append(s9)
+let s10 = SalaKonf(newName: "Sala nr 10", reservation: res, t: 300)
+array.append(s10)
+
+//rozdzielenie do dwóch różnych tablic ze względu na rezerwacje
+for i in array{
+    if (i.state == "Reserved"){
+        arrayReserved.append(i)
+    } else {
+        arrayNotReserved.append(i)
+    }
+}
+
+print("Reserved list:")
+for i in arrayReserved{
+    i.description()
+}
+
+print("Not reserved list:")
+for i in arrayNotReserved{
+    i.description()
+}
